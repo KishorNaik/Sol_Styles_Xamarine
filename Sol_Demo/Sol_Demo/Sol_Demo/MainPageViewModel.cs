8395fb70304511a62d2b745a8beb578336ffc2d9
KishorNaik <kishor.naik011.net@gmail.com>
2020-01-18 16:32:58 +0530

    code commit

---
+++ Sol_Demo/Sol_Demo/Sol_Demo/MainPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Sol_Demo
+{
+    public class MainPageViewModel 
+    {
+
+        #region Constructor
+        public MainPageViewModel()
+        {
+            OnSubmitClick();
+        }
+        #endregion 
+
+        #region Public Method
+        public Command SubmitDynamicResource { get; set; }
+        #endregion
+
+        #region Private Method
+        private void OnSubmitClick()
+        {
+            SubmitDynamicResource = new Command<ContentPage>((contentPage) => 
+            {
+                contentPage.Resources["btnStyleOrangle"] = contentPage.Resources["btnStyleRed"];
+
+            });
+        }
+        #endregion 
+    }
+}
